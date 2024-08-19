0d0f2eb30e27df1d58a8561a69115a4aed924c57
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-08-19 15:25:25 +0100

    second commit

--- src/app/page.js
+++ src/app/page.js
@@ -2,6 +2,7 @@ export default function Home() {
   return (
     <div>
       <h2>I am the home page and hopefully, I am on Vercel!</h2>
+      <p>Fingers crossed this time...</p>
     </div>
   );
 }
