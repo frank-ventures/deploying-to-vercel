6e933b2c30872723cc4b2e7b89abf3d959c95861
frank-ventures <6458936+frank-ventures@users.noreply.github.com>
2024-08-19 15:36:02 +0100

    I made an about page. Woo!

---
+++ src/app/about/page.js
@@ -0,0 +1,8 @@
+export default function AboutPage() {
+  return (
+    <>
+      <h2>I am the about page</h2>
+      <p>Why am I here? I am testing out branches. Git branches that is.</p>
+    </>
+  );
+}
